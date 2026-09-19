e3bf82a1249f99489709a41eafe10cdff9ec2e71
Abdennour Hachemi <tamokradz@gmail.com>
2026-09-19 23:41:44 +0100

    solving exercise 3-to-pounds

--- Sprint-2/3-mandatory-interpret/3-to-pounds.js
+++ Sprint-2/3-mandatory-interpret/3-to-pounds.js
@@ -6,10 +6,11 @@ const penceStringWithoutTrailingP = penceString.substring(
 );
 
 const paddedPenceNumberString = penceStringWithoutTrailingP.padStart(3, "0");
-const pounds = paddedPenceNumberString.substring(
-  0,
+
+const pounds = paddedPenceNumberString.substring( 0,
   paddedPenceNumberString.length - 2
 );
+console.log(pounds)
 
 const pence = paddedPenceNumberString
   .substring(paddedPenceNumberString.length - 2)
@@ -25,3 +26,8 @@ console.log(`£${pounds}.${pence}`);
 
 // To begin, we can start with
 // 1. const penceString = "399p": initialises a string variable with the value "399p"
+// 2. penceStringWithoutTrailingP = penceString.substring(  0,  penceString.length - 1): This line of code slice the penceString varible by removing the "p" character from it.
+// 3. const paddedPenceNumberString = penceStringWithoutTrailingP.padStart(3, "0") : This line of code adds "0" if lenght of penceStringWithoutTrailingP is less then 3 i.e e value is "1" => it will convert it to"001" or ifthe value is "22" it will confert it to "022".
+// 4. const pounds = paddedPenceNumberString.substring( 0,  paddedPenceNumberString.length - 2); : This line of code extract and store the value  of pound from paddedPenceNumberString using the substring method since £1 = 100 pences => any number in the hudered position i.e third character from the right onwards has a value of £.
+// 5. const pence = paddedPenceNumberString   .substring(paddedPenceNumberString.length - 2)  .padEnd(2, "0");: This line of code extract and store the value  of pences paddedPenceNumberString using the substring method => instead of starting from the left it start from the right moving two positions then it adds  "0"in case the pences value is less then 10.
+// 6. console.log(`£${pounds}.${pence}`): This line of code prints the final result in $ and pences in nice readable way => £3.99
