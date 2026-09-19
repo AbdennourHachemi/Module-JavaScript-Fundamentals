dc792ba7e29a5ec3df80f863ad2debbc1075b4b3
Abdennour Hachemi <tamokradz@gmail.com>
2026-09-19 14:34:54 +0100

    solving exercise 4-random

--- Sprint-2/1-key-exercises/4-random.js
+++ Sprint-2/1-key-exercises/4-random.js
@@ -7,3 +7,29 @@ const num = Math.floor(Math.random() * (maximum - minimum + 1)) + minimum;
 // Try breaking down the expression and using documentation to explain what it means
 // It will help to think about the order in which expressions are evaluated
 // Try logging the value of num and running the program several times to build an idea of what the program is doing
+
+//console.log(num);
+
+//First operation 
+// num is generating  a random floating number with method Math.random() between 0 and 1
+
+let test = Math.random()
+console.log(test)
+
+//Second operation
+//The generated number is multiplied by 100 being (maximum =100 -1 +1)
+
+ test =Math.random() *(maximum - minimum +1);
+ console.log(test)
+
+ //Third operation 
+ //num is rounded down to the nearset whole number using the method Math.floor
+
+  test = Math.floor(Math.random() * (maximum - minimum + 1)) 
+  console.log(test)
+
+  // Last operation is to add 1 to num 1 is stored in a variable called minimum
+
+  test =Math.floor(Math.random() * (maximum - minimum + 1)) + minimum;
+ console.log(test)
+ console.log(num)
