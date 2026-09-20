d386bfb42c2ecb5d571ac8f0dec7676e87e86fa6
Abdennour Hachemi <tamokradz@gmail.com>
2026-09-20 15:47:37 +0100

    correcting exercise 1.js

--- Sprint-2/2-mandatory-errors/1.js
+++ Sprint-2/2-mandatory-errors/1.js
@@ -3,5 +3,9 @@
 let age = 33;
 age = age + 1;
 
+/*
+Answer : The Error is  : TypeError: Assignment to constant variable.
+Constants in JavaScript can't be reassigned to correct this we have to change the varible type from const => let.
 
+*/
 console.log(age)
